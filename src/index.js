88e0d048690817b1f0c0ba325ad645bd2983597e
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-02-02 23:27:23 -0500

    assignment complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const form = document.querySelector('#create-task-form');
+  const taskInput = form.querySelector('#new-task-description')
+  const taskList = document.querySelector('#list');
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    addListItem(taskInput.value);
+    taskInput.value = '';
+  })
+
+  function addListItem(item) {
+    let li = document.createElement('li');
+    li.textContent = item;
+    addDelete(li);
+    taskList.append(li);
+  }
+
+  function addDelete(item) {
+    let span = document.createElement('span');
+    span.className = 'delete';
+    span.textContent = '\u{02715}';
+    span.addEventListener('click', () => span.parentNode.remove())
+    item.append(span);
+  }
+
 });
